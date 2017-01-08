ea64836b8520de0e833eb776ca1e5cdad4005458
Damien Roux <da4throux@users.noreply.github.com>
2017-01-08 23:53:12 +0100

    Update MMM-Paris-RATP-PG.js
    feature conversion of a time to a waiting time
    Activated through configuration convertToWaitingTime, convert hh:mm to 'dans mm mn'

--- MMM-Paris-RATP-PG.js
+++ MMM-Paris-RATP-PG.js
@@ -76,6 +76,15 @@ Module.register("MMM-Paris-RATP-PG",{
         if (!this.busSchedules[stopIndex]) {
           depCell.innerHTML = "N/A ";
         } else {
+          if (this.config.convertToWaitingTime && /^\d{1,2}[:][0-5][0-9]$/.test(comingBus.message)) {
+            var transportTime = comingBus.message.split(':');
+            var endDate = new Date(0, 0, 0, transportTime[0], transportTime[1]);
+            var now = new Date();
+            var startDate = new Date(0, 0, 0, now.getHours(), now.getMinutes(), now.getSeconds());
+            var waitingTime = endDate - startDate; 
+            if (startDate > endDate) { waitingTime += 1000 * 60 * 60 * 24);
+            comingBus.message.split('dans ' + waitingTime + ' mn');
+          }
           depCell.innerHTML = comingBus.message;
         }
         row.appendChild(depCell);
